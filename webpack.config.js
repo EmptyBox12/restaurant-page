f23a3094fb2e9d511f0de2eee331ed600a946a4d
EmptyBox12 <ataberktumay@gmail.com>
2021-09-02 13:37:02 +0300

    mode change

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/index.js',
-    devtool: 'inline-source-map',
+    
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
